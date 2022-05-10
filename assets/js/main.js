f804c2b158f4b2cb56d6179b97c9b307bf8517c4
Bakhtiyar Amrahov <bakhtiyaria@code.edu.az>
2022-05-10 09:57:33 +0400

    ProductDetail

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
         $(".owl-carousel").owlCarousel();
       });
 
-      $('.owl-carousel').owlCarousel({
+      $('.owl-carusel-last-seen').owlCarousel({
         loop:true,
         margin:10,
         nav:true,
@@ -19,5 +19,28 @@ $(document).ready(function(){
                 items:4
             }
         }
+
+       
     })
+
+    $('.owl-carousel-product').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            768:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+
+       
+    })
+
+    
   });
